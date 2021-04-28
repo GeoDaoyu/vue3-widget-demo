b5c1ee7aea2fc2e0188b934805fae3020f6c78d1
zhanggy <geodaoyu@foxmail.com>
2021-04-28 14:37:14 +0800

    zoom widget

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,88 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <div id="viewDiv"></div>
+    <div class="widgets">
+      <div id="zoom"></div>
+      <div id="zoom-wm">
+        <div class="esri-component esri-zoom esri-widget">
+          <div
+            class="esri-widget--button esri-widget esri-interactive"
+            role="button"
+            tabindex="0"
+            title="放大"
+            v-on:click="zoomIn"
+          >
+            <span
+              aria-hidden="true"
+              role="presentation"
+              class="esri-icon esri-icon-plus"
+            >
+            </span>
+            <span class="esri-icon-font-fallback-text">放大</span>
+          </div>
+          <div
+            class="esri-widget--button esri-widget esri-interactive"
+            role="button"
+            tabindex="-1"
+            title="缩小"
+            v-on:click="zoomOut"
+          >
+            <span
+              aria-hidden="true"
+              role="presentation"
+              class="esri-icon esri-icon-minus"
+            >
+            </span>
+            <span class="esri-icon-font-fallback-text">缩小</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import EsriMap from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
+import Zoom from '@arcgis/core/widgets/Zoom';
+import ZoomViewModel from '@arcgis/core/widgets/Zoom/ZoomViewModel';
 
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
+  zoomVM: null,
   methods: {
     initMap() {
       const map = new EsriMap({
         basemap: 'topo-vector',
       });
 
-      // eslint-disable-next-line no-unused-vars
       const view = new MapView({
-        container: 'viewDiv',
         map,
+        container: 'viewDiv',
       });
+
+      view.when((currView) => {
+        this.addZoomByWidget(currView);
+        this.addZoomByVM(currView);
+      });
+    },
+    addZoomByWidget(view) {
+      // eslint-disable-next-line no-unused-vars
+      const zoom = new Zoom({
+        view,
+        container: 'zoom',
+      });
+    },
+    addZoomByVM(view) {
+      this.zoomVM = new ZoomViewModel({
+        view,
+      });
+    },
+    zoomIn() {
+      this.zoomVM.zoomIn();
+    },
+    zoomOut() {
+      this.zoomVM.zoomOut();
     },
   },
   mounted() {
@@ -40,5 +98,13 @@ export default {
   height: 500px;
   width: 500px;
   margin: 0 auto;
+}
+.widgets {
+  position: absolute;
+  border: 1px black solid;
+  top: 60px;
+  left: 100px;
+  height: 500px;
+  width: 100px;
 }
 </style>
